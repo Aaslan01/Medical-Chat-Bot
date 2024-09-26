44d84a51ff2607785b831724aa0697d23d5fa5c1
Ali Sher Khan <72862093+Aaslan01@users.noreply.github.com>
2024-09-25 23:37:30 -0400

    Updated Requirement and added required things

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Medical Chat Bot',
+    version= '0.0.0',
+    # author= 'Bappy Ahmed',
+    # author_email= 'entbappy73@gmail.com',
+    # packages= find_packages(),
+    install_requires = []
+)
